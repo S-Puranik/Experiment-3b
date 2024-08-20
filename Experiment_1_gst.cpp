78b64f89dde3b579953fe31ec022039305b45a18
S-Puranik <132969156+S-Puranik@users.noreply.github.com>
2024-08-20 12:54:55 +0530

    Create Experiment_1_gst.cpp

---
+++ Experiment_1_gst.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    /* Input = Amount to be taxed as per the GST rate.
+       Output = The amount of GST applied on the amount, price before GST and price after GST.
+    */
+    
