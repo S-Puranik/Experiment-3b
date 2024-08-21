44d644ad44c202a6ce2926ef941dfc3e7c449634
S-Puranik <132969156+S-Puranik@users.noreply.github.com>
2024-08-21 22:41:29 +0530

    Update Exp7_arithmetic-input.cpp

--- Exp7_arithmetic-input.cpp
+++ Exp7_arithmetic-input.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    
+    
+    char arithmetic; 
+    double a, b, operation;
+
+
+    cout << "Enter an arithmetic operator such as +, -, *, and / : ";
+    cin >> arithmetic;
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    switch(arithmetic) {
+        case '+':
+            operation = a + b;
+            cout << "\nThe operation of " << a << " + " << b << " is: " << operation;
+            break;
+        case '-':
+            operation = a - b;
+            cout << "\nThe operation of " << a << " - " << b << " is: " << operation;
+            break;
+        case '*':
+            operation = a * b;
+            cout << "\nThe operation of " << a << " * " << b << " is: " << operation;
+            break;
+        case '/':
+            operation = a / b;
+            cout << "\nThe operation of " << a << " / " << b << " is: " << operation;
+            break;
+        default:
+            cout << "\nError: Invalid operator. Please use +, -, *, or /.";
+
+    }
+
+    return 0;
+
+}
